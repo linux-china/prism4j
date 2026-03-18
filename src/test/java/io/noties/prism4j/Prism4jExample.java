b186a448d3f770724834f4153cb7b20463099b78
Liging Chen <libing.chen@gmail.com>
2026-03-18 10:33:01 +0800

    chore: use java code

--- src/test/java/io/noties/prism4j/Prism4jExample.java
+++ src/test/java/io/noties/prism4j/Prism4jExample.java
@@ -4,15 +4,15 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-/**
- * @author lihua.llh
- */
 public class Prism4jExample {
   public static void main(String[] args) {
-    String code = "# DEMO";
+    String language = "java";
+    String code = "public class Hello { public static void main(String[] args) { System.out.println(\"Hello world!\"); } }";
     final Prism4j prism4j = new Prism4j(new DefaultGrammarLocator());
-    final Prism4j.Grammar grammar = prism4j.grammar("markdown");
+    final Prism4j.Grammar grammar = prism4j.grammar("java");
     StringBuilder sb = new StringBuilder();
+    // add prism html structure
+    sb.append(String.format("<pre class=\"language-%s\"><code class=\"language-%s\">\n", language, language));
     if (grammar != null) {
       final List<Prism4j.Node> nodes = prism4j.tokenize(code, grammar);
       final AbsVisitor visitor = new AbsVisitor() {
@@ -20,20 +20,21 @@ public class Prism4jExample {
         @Override
         protected void visitText(@NotNull Prism4j.Text text) {
           // raw text
-          text.literal();
           sb.append(text.literal());
         }
 
         @Override
         protected void visitSyntax(@NotNull Prism4j.Syntax syntax) {
           // type of the syntax token
-          sb.append("<span class=\"").append(syntax.type()).append("\">");
+          final String tokenType = syntax.type();
+          sb.append("<span class=\"token ").append(tokenType).append("\">");
           visit(syntax.children());
           sb.append("</span>");
         }
       };
       visitor.visit(nodes);
     }
+    sb.append("\n</code></pre>");
     System.out.println(sb);
   }
 }
